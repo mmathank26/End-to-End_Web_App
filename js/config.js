fabdfdd310b3e0677d56c5637e51428231bb848f
mmathank26 <meghalmathankar@gmail.com>
2025-09-17 23:56:58 +0530

    added temp creds for AWS >> PoolID and ClientID

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Q4KDMDzXN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6adumjk5if191nfasgfl7d6h4m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
