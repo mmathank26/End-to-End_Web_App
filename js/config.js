fa355e5330f2a8cb6cdac78496dbfcbac5ab1cd5
mmathank26 <meghalmathankar@gmail.com>
2025-09-28 17:23:53 +0530

    updated the config.js file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lzVFG0ZBu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'u9h65ta5ht6bl8imrmai0jql0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_KSkqJDKH1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5m7mgso2b8r4r9lm0mmv6nolf7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+ 
